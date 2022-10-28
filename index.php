f0a0d60cfa7746a24db04f8759eceb27bdd76db9
pescharatk <116812127+pescharatk@users.noreply.github.com>
2022-10-28 11:00:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -115,4 +115,5 @@ foreach ($pageData as $key => $value) {
     
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     </body>
+ 
 </html>             
